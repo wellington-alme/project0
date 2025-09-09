6e1107d66a7fc689f28ae28491c743cff8ef33dc
wellington-alme <wellington.santos6a@gmail.com>
2025-09-09 19:41:18 -0300

    envio dos arquivos

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import MeuComponente from './components/MeuComponente/MeuComponente.vue'
+
+const nome = ref('')
+const email = ref('')
+const telefone = ref('')
+const endereco = ref('')
+const cpf = ref('')
+
+const cadastro = ref({
+  nome: '',
+  email: '',
+  telefone: '',
+  endereco: '',
+  cpf: ''
+})
+
+function cadastrar() {
+  cadastro.value = {
+    nome: nome.value,
+    email: email.value,
+    telefone: telefone.value,
+    endereco: endereco.value,
+    cpf: cpf.value
+  }
+}
+</script>
+
+<template>
+  <div>
+    <input type="text" placeholder="Nome" v-model="nome"/><br>
+    <input type="text" placeholder="E-mail" v-model="email"/><br>
+    <input type="text" placeholder="Telefone" v-model="telefone"/><br>
+    <input type="text" placeholder="Endereço" v-model="endereco"/><br>
+    <input type="text" placeholder="CPF" v-model="cpf"/><br>
+    <button @click="cadastrar">CADASTRAR</button>
+    <MeuComponente
+      :nome="cadastro.nome"
+      :email="cadastro.email"
+      :telefone="cadastro.telefone"
+      :endereco="cadastro.endereco"
+      :cpf="cadastro.cpf"
+    />
+  </div>
+</template>
+
+<style scoped>
+</style>  
---
+++ src/components/MeuComponente/MeuComponente.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="nome || email || telefone || endereco || cpf">
+    <h3>Informações Cadastradas:</h3>
+    <p><strong>Nome:</strong> {{ nome }}</p>
+    <p><strong>E-mail:</strong> {{ email }}</p>
+    <p><strong>Telefone:</strong> {{ telefone }}</p>
+    <p><strong>Endereço:</strong> {{ endereco }}</p>
+    <p><strong>CPF:</strong> {{ cpf }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  nome: string
+  email: string
+  telefone: string
+  endereco: string
+  cpf: string
+}>()
+</script>
